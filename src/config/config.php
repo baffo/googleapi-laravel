c2da6c0adb49201f2d197693406b7b5dadc258bb
baffo <primoz.bevk@gmail.com>
2014-09-03 11:24:36 +0200

    multiple client API ids

--- src/config/config.php
+++ src/config/config.php
@@ -1,11 +1,21 @@
 <?php
-
 return array(
 
-    // OAuth2 Setting, you can get these keys in Google Developers Console
-    'oauth2_client_id'      => '< YOUR CLIENT ID >',
-    'oauth2_client_secret'  => '< YOUR CLIENT SECRET >',
-    'oauth2_redirect_uri'   => 'http://localhost:8000/',   // Change it according to your needs
+	'auth' => array(
+		'local' => array(
+			// OAuth2 Setting, you can get these keys in Google Developers Console
+            'oauth2_client_id'      => '< YOUR CLIENT ID >',
+            'oauth2_client_secret'  => '< YOUR CLIENT SECRET >',
+            'oauth2_redirect_uri'   => 'http://localhost:8000/',   // Change it according to your needs
+		),
+		'prod' => array(
+			// OAuth2 Setting, you can get these keys in Google Developers Console
+            'oauth2_client_id'      => '< YOUR CLIENT ID >',
+            'oauth2_client_secret'  => '< YOUR CLIENT SECRET >',
+            'oauth2_redirect_uri'   => 'http://localhost:8000/',   // Change it according to your needs
+		)
+	
+	),
 
     // Definition of service specific values like scopes, OAuth token URLs, etc
     'services' => array(
@@ -13,6 +23,7 @@ return array(
         'calendar' => array(
             'scope' => 'https://www.googleapis.com/auth/calendar'
         ),
+		'https://www.googleapis.com/auth/userinfo.email'
         /*'books' => [
             'scope' => 'https://www.googleapis.com/auth/books'
         ]*/
@@ -23,7 +34,7 @@ return array(
     'service_class_prefix' => 'Google_Service_',
 
     // Custom settings
-    'access_type' => 'online',    
+    'access_type' => 'offline',    
     'approval_prompt' => 'auto',
 
 );
